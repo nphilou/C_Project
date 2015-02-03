7f6d8474277484de208ce95dc46af6fdb3400717
Lea <leanaccache@hotmail.fr>
2015-02-03 11:26:26 +0100

    test

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct{
+int x;
+int y;
+ } Coordonnee;
+
+typedef struct {
+Fourmiliere * rouge;
+Fourmiliere * noire;
+
+
+}Monde;
+
+
+typedef struct {
+ char couleur;
+ char type [3];
+ Coordonnee destination;
+ Coordonnee position;
+ int temps;
+ Fourmiliere * origine;
+ Fourmi * suiv;
+} Fourmi;
+
+
+
+
+
+int main()
+{
+
+    return 0;
+}
+
+
+
