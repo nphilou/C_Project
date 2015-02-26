9642265b3c8f3fc9353a94262b458a98dafbe8fc
philippe <nphilou@gmail.com>
2015-02-26 22:16:51 +0100

    sh

--- fourmidable.sh
+++ fourmidable.sh
@@ -4,4 +4,4 @@ echo "-> COMPILATION\n"
 gcc -Wall plateau.c init.c combat.c deplacement.c instructions.c app.c fourmido.c -c -g -lm 2> logs/compile.log 
 gcc plateau.o init.o combat.o deplacement.o instructions.o app.o fourmido.o -o fourmidable -lm 2> logs/link.log
 echo "-> EXECUTION\n"
-valgrind ./fourmidable --leak-check=full
+valgrind ./fourmidable
