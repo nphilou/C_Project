02f01342e0f4cbf631f7e910e1e79431032dba9b
philippe <nphilou@gmail.com>
2015-03-01 00:15:49 +0100

    minor fixes

--- deplacement.h
+++ deplacement.h
@@ -1,19 +1,13 @@
-#include "combat.h"
+#ifndef DEF_DEPLACEMENT
+#define DEF_DEPLACEMENT
 
-void demandeDestination(int *abscisse, int *ordonnee, Monde *monde, Fourmi *fourmi);
+    void demandeDestination(int *abscisse, int *ordonnee, Monde *monde, Fourmi *fourmi);
+    void deplacementFourmi(Monde *myWorld, Fourmi *fourmi, int abscisse, int ordonnee);
+    void deplacementNord(Monde *myWorld, Fourmi *fourmi, int abscisse);
+    void deplacementSud(Monde *myWorld, Fourmi *fourmi, int abscisse);
+    void deplacementOuest(Monde *myWorld, Fourmi *fourmi, int ordonnee);
+    void deplacementEst(Monde *myWorld, Fourmi *fourmi, int ordonnee);
+    void ajoutFourmi(Monde *myWorld, Fourmi *fourmi, int indice);
+    void combatCase(Monde *myWorld, Fourmi *fourmi, int indice);
 
-void deplacementFourmi(Monde *myWorld, Fourmi *fourmi, int abscisse, int ordonnee);
-
-
-void deplacementNord(Monde *myWorld, Fourmi *fourmi, int abscisse);
-
-void deplacementSud(Monde *myWorld, Fourmi *fourmi, int abscisse);
-
-void deplacementOuest(Monde *myWorld, Fourmi *fourmi, int ordonnee);
-
-void deplacementEst(Monde *myWorld, Fourmi *fourmi, int ordonnee);
-
-
-void ajoutFourmi(Monde *myWorld, Fourmi *fourmi, int indice);
-
-void combatCase(Monde *myWorld, Fourmi *fourmi, int indice);
+#endif
